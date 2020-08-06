3e79677ed1617eee8cf08210d263e4342b0c97cb
glassbones <glassbonesofficial@gmail.com>
2020-08-06 10:18:38 -0500

    global css resets

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,33 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
-import Theme from ".src/themes/theme"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
 
 //this wraps the entire root with styled components themeProvider
 // element = react app
 
+
+// this is global css styling resets
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1}
+    }
+`
+//jsx return
 export const wrapRootElement = ( { element } ) => (
 
-    <ThemeProvider theme={Theme}> {element} </ThemeProvider>
+    <ThemeProvider theme={Theme}>
+
+        <GlobalStyles/>
+        {element}
+
+    </ThemeProvider>
 
 )
